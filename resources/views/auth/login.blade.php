03124c79830c19624b7843f35b315526a5b0661c
Billy ROUSSEAU <bonjour@dwenola.xyz>
2024-07-17 16:45:58 +0200

    design: login page finished template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,33 +9,36 @@
 
     @vite('css')
 </head>
-<body class="h-screen">
-<div class="h-screen w-full flex flex-col gap-6 justify-center items-center">
-    <h1 class="text-3xl font-bold text-center">
-        Streamboard -|- Tableau de bord
-    </h1>
-    <div class="w-[500px] bg-black rounded-lg p-4 gap-4 flex flex-col">
-        <form action="{{ route('login') }}" method="POST" class="w-full flex flex-col gap-4">
+<body class="h-full">
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div class="text-center"><i class="fa fa-rocket text-4xl text-emerald-500"></i></div>
+        <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Connexion à Streamboard</h2>
+    </div>
+
+    <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="{{ route('login') }}" method="POST">
             @csrf
-            <input type="text" name="email"
-                   class="block w-full rounded-md border-0 py-2 pl-7 pr-20 text-gray-900 font-light placeholder:text-gray-400"
-                   placeholder="example@domaine.com"
-                   required/>
-            <input type="password" name="password"
-                   class="block w-full rounded-md border-0 py-2 pl-7 pr-20 text-gray-900 placeholder:text-gray-400"
-                   placeholder="Mot de passe"
-                   required/>
-            <div class="">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox"
-                           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
-                           name="remember">
-                    <span class="ms-2 text-white">Se rappeler de moi</span>
-                </label>
+            <div>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Adresse e-mail</label>
+                <div class="mt-2">
+                    <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6 pl-3">
+                </div>
             </div>
-            <button type="submit" class="w-full p-2 bg-white hover:bg-white/95 text-black rounded-md text-center font-bold">
-                Se connecter
-            </button>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Mot de passe</label>
+                </div>
+                <div class="mt-2">
+                    <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6 pl-3">
+                </div>
+            </div>
+
+            <div>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">Sign in</button>
+            </div>
         </form>
     </div>
 </div>
